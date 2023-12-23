6d03da4f84908f5b496a81706f491004c6c72cac
mrobval <mrobe2503@icloud.com>
2023-12-23 20:20:33 +0100

    Ficheros BSNode y BSTree completados

--- BSTree.h
+++ BSTree.h
@@ -27,7 +27,7 @@ class BSTree {
 	};
 	BSNode<T>* insert(BSNode<T>* n, T e){
 		if (n == nullptr){
-			new BSNode<T>*;
+			return new BSNode<T>(e);
 		}
 		else if (n->elem == e){
 			throw std::runtime_error("Ya existe ese elemento en el ABB");
@@ -40,16 +40,15 @@ class BSTree {
 		}
 		return n;
 	};
-	void print_inorder(std::ostream& out, BSNode<T> n) const{
-		if (n == nullptr){
-			throw std::runtime_error("No hay elementos");
-		}
-		else if (n.left != nullptr){
-			print_inorder(out, n->left);
-		}
-		print_inorder(out, n);
-		if (n.right != nullptr){
-			print_inorder(out, n->right);
+	void print_inorder(std::ostream &out, BSNode<T>* n) const{
+		if (n != nullptr) {
+			if (n->left != nullptr) {
+				print_inorder(out, n->left);
+			}
+			out<< n->elem<< " ";
+			if (n->right != nullptr) {
+				print_inorder(out, n->right);
+			}
 		}
 	};
 	BSNode<T>* remove(BSNode<T>* n, T e){
@@ -92,42 +91,45 @@ class BSTree {
 			n->right = remove_max(n->right);
 			return n;
 		}
+		delete n;
 	};
 	void delete_cascade(BSNode<T>* n){
-		if (n == nullptr){
-			throw std::runtime_error("El arbol ya está vacío");
+		if (n != nullptr){
+			if (n->left != nullptr){
+				delete_cascade(n->left);
+			}
+			else if (n->right != nullptr){
+				delete_cascade(n->right);
+			}
+		delete n;
 		}
-		else if (n->left != nullptr){
-			delete_cascade(n->left);
-		}
-		else if (n->right != nullptr){
-			delete_cascade(n->right);
-		}
-		delete [] n;
 	};
     public:
         // miembros públicos
 	BSTree(){
 		root = nullptr;
+		nelem = 0;
 	};
 	int size() const{
 		return nelem;
 	};
 	T search(T e) const{
-		search(root, e);
+		return search(root, e)->elem;
 	};
 	T operator[](T e)const{
-		search(e);
+		return search(e)->elem;
 	};
 	void insert (T e){
 		root = insert(root, e);
+		nelem++;
 	};
 	friend std::ostream&operator<<(std::ostream &out, const BSTree<T> &bst){
-		bst.print_inorder(out, bst.nelem);
+		bst.print_inorder(out, bst.root);
 		return out;
 	};
 	void remove (T e){
 		root = remove(root, e);
+		nelem--;
 	};
 	~BSTree(){
 		delete_cascade(root);
